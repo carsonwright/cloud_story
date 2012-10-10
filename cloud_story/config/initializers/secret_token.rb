2ca767253d4f419ad8a87d01bd1c92b1c8de1e5c
Carson Wright <carson@woofound.com>
2012-10-09 21:21:23 -0400

    Initialize app and create users, roles, and story

---
+++ cloud_story/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CloudStory::Application.config.secret_token = '00cab9e8c96cfef9937a60284d4af697ad48c19dcf17ff1209de6de0ba47134cfcf9127893bbbdcbc1dbc446de72062f0134129b9ea2ee0850d932c1afda8f5a'
